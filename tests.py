7d5bb0104ef0e0e4056471b4aaa7dfcd227ff426
sretalla <43703122+sretalla@users.noreply.github.com>
2019-07-27 23:44:21 +0200

    Updated test cases for vignette system

--- tests.py
+++ tests.py
@@ -434,3 +434,23 @@ class TestWRC():
             route = wrc.WazeRouteCalculator(from_address, to_address, avoid_ferries=False)
             route.get_route()
         assert 'avoid_ferries' not in req.last_request.query
+    
+    def test_avoid_toll_road_true_vignette(self):
+        from_address = 'From address'
+        to_address = 'To address'
+        with requests_mock.mock() as m:
+            m.get(self.address_req, text=self.address_to_coords_response)
+            req = m.get(self.routing_req, text=self.routing_response)
+            route = wrc.WazeRouteCalculator(from_address, to_address, avoid_toll_roads=True)
+            route.get_route()
+        assert 'subscription' in req.last_request.query
+
+    def test_avoid_toll_road_false_vignette(self):
+        from_address = 'From address'
+        to_address = 'To address'
+        with requests_mock.mock() as m:
+            m.get(self.address_req, text=self.address_to_coords_response)
+            req = m.get(self.routing_req, text=self.routing_response)
+            route = wrc.WazeRouteCalculator(from_address, to_address, avoid_toll_roads=False)
+            route.get_route()
+        assert 'subscription' not in req.last_request.query
